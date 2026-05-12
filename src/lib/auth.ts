f6c783810003566d8464cb22f438f8c400dec089
AbdurRahman11072 <rjrahman014@gmail.com>
2026-05-12 13:51:20 +0600

    refactor auth configuration to simplify baseURL and remove advanced cookie attributes for improved compatibility

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,8 +5,8 @@ import { prisma } from "./prisma";
 // If your Prisma file is located elsewhere, you can change the path
 
 export const auth = betterAuth({
-  baseURL: `${process.env.BACKEND_URL}/api/auth`,
-  trustedOrigins: [`${process.env.FRONTEND_URL}`, "http://localhost:3000"],
+  baseURL: `${process.env.FRONTEND_URL}`,
+
   database: prismaAdapter(prisma, {
     provider: "postgresql", // or "mysql", "postgresql", ...etc
   }),
@@ -27,17 +27,6 @@ export const auth = betterAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
   },
-  advanced: {
-    // Remove crossSubDomainCookies — it only works for subdomains
-    // (like app.example.com → api.example.com), not for completely
-    // different domains like yours.
 
-    defaultCookieAttributes: {
-      sameSite: "none", // allows cross-site cookie sending
-      secure: true, // required whenever sameSite is "none"
-      httpOnly: true, // security best practice
-      partitioned: true, // for CHIPS / newer browser support
-    },
-  },
   plugins: [admin()],
 });
